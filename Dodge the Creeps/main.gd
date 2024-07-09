b56aa36d12a4df2ab97921ec9d52d473cd43e149
ixniz <50206812+ixniz@users.noreply.github.com>
2024-07-09 21:05:01 +0200

    Mobs spawn directed at the player

--- Dodge the Creeps/main.gd
+++ Dodge the Creeps/main.gd
@@ -37,14 +37,11 @@ func _on_mob_timer_timeout():
 	var mob_spawn_location = $MobPath/MobSpawnLocation
 	mob_spawn_location.progress_ratio = randf()
 
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
 	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
 
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
+	# Point the mob towards the Player
+	var direction = mob.position.angle_to_point($Player.global_position)
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
